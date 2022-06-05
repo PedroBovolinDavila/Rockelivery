9b2855b2120e33c645be28aec073e8c8aec6aee9
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-06-05 01:48:35 -0300

    :sparkles: upload project on github

---
+++ lib/rockelivery_web/views/users_view.ex
@@ -0,0 +1,21 @@
+defmodule RockeliveryWeb.UsersView do
+  use RockeliveryWeb, :view
+
+  alias Rockelivery.User
+
+  def render("create.json", %{user: %User{} = user}) do
+    %{
+      message: "User created!",
+      user: user
+    }
+  end
+
+  def render("user.json", %{user: %User{} = user}), do: %{user: user}
+
+  def render("delete.json", %{user: %User{} = user}) do
+    %{
+      message: "User deleted",
+      user: user
+    }
+  end
+end
