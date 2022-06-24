82d0891059a3aaef87ecb6418884362c94e119cb
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-06-24 17:39:03 -0300

    :heavy_plus_sign: Added Tesla http client

--- mix.exs
+++ mix.exs
@@ -45,7 +45,9 @@ defmodule Rockelivery.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:pbkdf2_elixir, "~> 2.0"}
+      {:pbkdf2_elixir, "~> 2.0"},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
